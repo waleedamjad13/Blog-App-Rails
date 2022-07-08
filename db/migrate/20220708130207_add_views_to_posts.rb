92818c6751714e2fb11dcd309696dcd844c561d4
Waleed Amjad <waleedamjad57@gmail.com>
2022-07-09 01:06:50 +0500

    Add Views to users profile

--- db/migrate/20220708130207_add_views_to_posts.rb
+++ db/migrate/20220708130207_add_views_to_posts.rb
@@ -1,5 +1,5 @@
 class AddViewsToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_column :posts, :views, :integer, default: 0 # it is for vies of posts means how many vies your posts got
+    add_column :posts, :views, :integer, default: 0 # it is for views of posts means how many views your posts got
   end
 end
